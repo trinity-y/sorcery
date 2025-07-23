a64e6a6522ff78e7f56263d33212c48933763364
trinity-y <tayip@uwaterloo.ca>
2025-07-23 13:11:30 -0400

    starting to build player

---
+++ player.h
@@ -0,0 +1,14 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <memory>
+using namespace std;
+#include "./zones/board.h"
+#include "./zones/deck.h"
+#include "./zones/hand.h"
+#include "./zones/graveyard.h"
+
+class Player {
+
+};
+
+#endif
