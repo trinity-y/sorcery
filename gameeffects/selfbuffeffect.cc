209072d4a5691f7964fb6cf7bd31e002d797b4ca
Celina He <90709918+celina-he@users.noreply.github.com>
2025-07-28 23:04:05 -0400

    Update selfbuffeffect.cc

--- gameeffects/selfbuffeffect.cc
+++ gameeffects/selfbuffeffect.cc
@@ -5,21 +5,9 @@
 SelfBuffEffect::SelfBuffEffect(int attackChange, int defenseChange)
     : attackChange{attackChange}, defenseChange{defenseChange} {}
 
-void SelfBuffEffect::useEffect()
-{
-    // Default useEffect() - this version doesn't have enough context
-    // The useEffect(Player &p, int minionIndex) version should be used instead
-}
-
-void SelfBuffEffect::useEffect(Player &p)
-{
-    // This version doesn't know which specific minion to target
-    // The useEffect(Player &p, int minionIndex) version is more useful
-}
-
 void SelfBuffEffect::useEffect(Player &p, int minionIndex)
 {
-    // This is the ideal version - buff the specific minion at the given index
+    // buff the specific minion at the given index
     // Get current stats
     int currentAttack = p.getMinionAttack(minionIndex);
     int currentDefense = p.getMinionDefence(minionIndex);
